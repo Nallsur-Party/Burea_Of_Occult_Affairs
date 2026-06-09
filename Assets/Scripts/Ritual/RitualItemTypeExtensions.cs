b25411a5cd9c32b03b6b3de70f0e410044d9f20e
nallsur <nallsurshow@gmail.com>
2026-06-09 23:55:31 +0900

    fix bugs

--- Assets/Scripts/Ritual/RitualItemTypeExtensions.cs
+++ Assets/Scripts/Ritual/RitualItemTypeExtensions.cs
@@ -16,4 +16,21 @@ public static class RitualItemTypeExtensions
                 return item.ToString();
         }
     }
+
+    public static string GetDescription(this RitualItemType item)
+    {
+        switch (item)
+        {
+            case RitualItemType.GlassWithPencil:
+                return "Поднести к рту, постучать карандашом, держать перед лицом.";
+            case RitualItemType.SoapWithPlantain:
+                return "Круговые движения мылом для намыливания, приложить к точке.";
+            case RitualItemType.GildedIcon:
+                return "Удерживать перед собой и вращать, как фонарик, чтобы просветить комнату.";
+            case RitualItemType.LeadTablet:
+                return "Поднести к пациенту и следить за прогресс-баром.";
+            default:
+                return item.ToString();
+        }
+    }
 }
